48c102dac0e9c7d96c5129080318811fd735ff67
Pedro <pedrogoku80@gmail.com>
2024-09-27 17:57:19 -0300

    Modificado

--- Capitulo16/createSort.go
+++ Capitulo16/createSort.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+type carro struct {
+	nome string
+	potencia float64
+	consumo float64
+}
+
+type ordenarPorPotencia [] carro
+type ordenarPorConsumo [] carro
+type ordenarPorLucroProDono [] carro
+
+func (x ordenarPorPotencia) Len() int {
+	return len(x)
+}
+
+func(x ordenarPorPotencia)  Less(i, j int) bool { 
+	return x[i].potencia < x[j].potencia
+}
+
+func (a ordenarPorPotencia) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (x ordenarPorConsumo) Len() int {
+	return len(x)
+}
+
+func(x ordenarPorConsumo)  Less(i, j int) bool { 
+	return x[i].consumo < x[j].consumo  
+}
+
+func (a ordenarPorConsumo) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (x ordenarPorLucroProDono) Len() int {
+	return len(x)
+}
+
+func(x ordenarPorLucroProDono)  Less(i, j int) bool { 
+	return x[i].consumo > x[j].consumo
+}
+
+func (a ordenarPorLucroProDono) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
 
 func main() {
-	fmt.Println("Hello Wolrd")
+	carros := []carro { carro{"chevete", 50, 5}, carro{"porsche", 300, 5}, carro{"fusca", 20, 30}}
+	fmt.Println(carros)
+	sort.Sort(ordenarPorPotencia(carros))
+	fmt.Println(carros)
+	sort.Sort(ordenarPorConsumo(carros))
+	fmt.Println(carros)
+	sort.Sort(ordenarPorLucroProDono(carros))
+	fmt.Println(carros)
 }
