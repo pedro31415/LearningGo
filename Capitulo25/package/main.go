1c4cc4e8942cabeafffb58fa91dd6f27aafeddc8
Pedro <pedrogoku80@gmail.com>
2024-11-05 20:51:02 -0300

    Capitulo 25

---
+++ Capitulo25/package/main.go
@@ -0,0 +1,14 @@
+package xelogers
+
+import "fmt"
+
+// Essa função não retorna nada
+func ruan() {
+	fmt.Println("Ruan está usando php")
+}
+
+// Essa função faz você virar nomade
+func antonio() {
+	fmt.Println("Viajando")
+}
+
