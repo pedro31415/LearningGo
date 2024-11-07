31483bf748137fbcd28c804eefad105cde998b47
Pedro <pedrogoku80@gmail.com>
2024-11-07 15:43:01 -0300

    study benchmark

--- Capitulo27/01_introducao/main_test.go
+++ Capitulo27/01_introducao/main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 type test struct {
 	data []int
@@ -12,7 +15,7 @@ func TestAllTesteSoma(t *testing.T) {
 	tests := []test {
 		test{data: []int{50,50}, answer: 100},
 		test{data: []int{30,50}, answer: 80},
-		test{data: []int{12,80}, answer: 93},
+		test{data: []int{12,80}, answer: 92},
 		test{data: []int{32,32}, answer: 64},
 	}
 
@@ -49,5 +52,17 @@ func TestSoma3(t *testing.T) {
 
 	if test != result {
 		t.Error("Expected: ", result, "but got: ", test)
+	}
+}
+
+func TestExemploSoma(t *testing.T) {
+	fmt.Println(sum(4,2,3))
+	// Output: 9
+}
+
+
+func BenchmarkSoma(b *testing.B) {
+	for i:= 0; i < b.N; i++  {
+		sum(23,50)
 	}
 }
